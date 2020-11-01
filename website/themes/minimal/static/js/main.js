da5e300138f67a7d2aa30e6acfde10ab3e998049
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2020-11-02 00:33:55 +0100

    Switch to the dist versions of the CSS and JS files for the vote links

--- website/themes/minimal/static/js/main.js
+++ website/themes/minimal/static/js/main.js
@@ -5,30 +5,7 @@ function init() {
     // Init the highlight.js library
     hljs.initHighlightingOnLoad();
 
-    // Click events for all vote links
-    Array.from(document.querySelectorAll("[data-vote]")).forEach(
-        (e) =>
-            (e.onclick = (event) => {
-                // Post the vote to the backend
-                const url = `https://iwanttoreadmore.com/vote/${e.getAttribute("data-vote")}`;
-                fetch(url, { method: "post" });
-
-                // Remove the link from the vote icon
-                e.removeAttribute("href");
-                e.onclick = null;
-                e.removeAttribute("data-vote");
-                e.style = "color: #4a5568;";
-
-                // Display the toast
-                const toast = document.getElementById("vote-toast");
-                toast.classList.remove("hidden");
-                setInterval(function () {
-                    toast.classList.add("hidden");
-                }, 10000);
-
-                return false;
-            })
-    );
+    iwanttoreadmore.init();
 }
 
 if (document.readyState !== "loading") {
