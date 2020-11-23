15843a9eee17c4b0d187a199b8197bc3597f82fd
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2020-11-23 23:56:15 +0100

    Add a navbar for logged in users
    - Add a modified navbar for logged in users (hidden by default)
    - Show the new navbar if the user is logged in

--- website/themes/minimal/static/js/main.js
+++ website/themes/minimal/static/js/main.js
@@ -2,10 +2,24 @@
  * Initializes all UI elements
  */
 function init() {
+    // Choose which navbar to show
+    chooseNavbar();
+
     // Init the highlight.js library
     hljs.initHighlightingOnLoad();
 
+    // Init the I Want To Read More script
     iwanttoreadmore.init();
+}
+
+/**
+ * Chooses which navbar to show depending on if the user is loggen in or not
+ */
+function chooseNavbar() {
+    if (document.cookie.includes("loggedin")) {
+        document.getElementById("logged-out-nav").classList.add("hidden");
+        document.getElementById("logged-in-nav").classList.remove("hidden");
+    }
 }
 
 if (document.readyState !== "loading") {
