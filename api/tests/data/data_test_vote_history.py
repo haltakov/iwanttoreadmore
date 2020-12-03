52573dbcae26856432a6135f0a5304b188c38c0a
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2020-12-04 00:01:00 +0100

    Implement the vote history table

---
+++ api/tests/data/data_test_vote_history.py
@@ -0,0 +1,72 @@
+import boto3
+from decimal import Decimal
+
+
+def create_vote_history_table(table_name):
+    """
+    Create test vote history table
+    :param table_name: name of the table
+    :return: the table object
+    """
+    dynamodb = boto3.resource("dynamodb")
+    return dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[{"AttributeName": "IPHash", "KeyType": "HASH"},],
+        AttributeDefinitions=[{"AttributeName": "User", "AttributeType": "N"},],
+        GlobalSecondaryIndexes=[
+            {
+                "IndexName": "UserTopicKey",
+                "KeySchema": [
+                    {"AttributeName": "User", "KeyType": "HASH"},
+                    {"AttributeName": "TopicKey", "KeyType": "RANGE"},
+                ],
+                "Projection": {
+                    "ProjectionType": "INCLUDE",
+                    "NonKeyAttributes": ["VoteTimestamp",],
+                },
+                "ProvisionedThroughput": {
+                    "ReadCapacityUnits": 1,
+                    "WriteCapacityUnits": 1,
+                },
+            },
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 2, "WriteCapacityUnits": 2},
+    )
+
+
+def create_test_vote_history_data(votes_table):
+    """
+    Populates the vote history table with test data
+    :param votes_table: table object
+    """
+    votes_table.put_item(
+        Item={
+            "User": "user_1",
+            "TopicKey": "project_a/topic_aaa",
+            "ProjectName": "project_a",
+            "Topic": "topic_aaa",
+            "VoteTimestamp": Decimal(1111),
+            "IPHash": hash("user_1" + "project_a/topic_aaa" + "192.168.0.1"),
+        }
+    )
+    votes_table.put_item(
+        Item={
+            "User": "user_1",
+            "TopicKey": "project_a/topic_aaa",
+            "ProjectName": "project_a",
+            "Topic": "topic_aaa",
+            "VoteTimestamp": Decimal(2222),
+            "IPHash": hash("user_1" + "project_a/topic_aaa" + "192.168.0.2"),
+        }
+    )
+    votes_table.put_item(
+        Item={
+            "User": "user_1",
+            "TopicKey": "project_a/topic_bbb",
+            "ProjectName": "project_a",
+            "Topic": "topic_bbb",
+            "VoteTimestamp": Decimal(3333),
+            "IPHash": hash("user_1" + "project_a/topic_bbb" + "192.168.0.2"),
+        }
+    )
+
---
+++ api/tests/models/test_vote_history.py
@@ -0,0 +1,94 @@
+import unittest
+import os
+from decimal import Decimal
+from unittest import mock
+from moto import mock_dynamodb2
+from iwanttoreadmore.models.vote_history import VoteHistory
+from tests.data.data_test_vote_history import (
+    create_vote_history_table,
+    create_test_vote_history_data,
+)
+from tests.helpers import remove_table
+
+
+@mock_dynamodb2
+class VoteHistoryTestCase(unittest.TestCase):
+    def setUp(self):
+        """
+        Create a vote history table and populate it with example data
+        """
+        os.environ["VOTE_HISTORY_TABLE"] = "iwanttoreadmore-vote-history-test"
+
+        self.vote_history_table = create_vote_history_table(
+            os.environ["VOTE_HISTORY_TABLE"]
+        )
+        create_test_vote_history_data(self.vote_history_table)
+
+    def tearDown(self):
+        remove_table(os.environ["VOTE_HISTORY_TABLE"])
+
+    def test_get_vote_history(self):
+        vote_history = VoteHistory()
+
+        # Positive cases
+        self.assertEqual(
+            [1111, 2222], vote_history.get_vote_history("user_1", "project_a/topic_aaa")
+        )
+        self.assertEqual(
+            [3333], vote_history.get_vote_history("user_1", "project_a/topic_bbb")
+        )
+
+        # Negative cases
+        self.assertEqual(
+            [], vote_history.get_vote_history("user_2", "project_a/topic_aaa")
+        )
+        self.assertEqual(
+            [], vote_history.get_vote_history("user_1", "project_a/topic_ccc")
+        )
+
+    @mock.patch("time.time", return_value=9999)
+    def test_add_vote_history(self, _):
+        vote_history = VoteHistory()
+
+        # Invalid vote
+        vote_history.add_vote_history("user_1", "project_a/topic_aaa", "192.168.0.1")
+        self.assertEqual(
+            [Decimal(1111), Decimal(2222)],
+            vote_history.get_vote_history("user_1", "project_a/topic_aaa"),
+        )
+
+        # Valid vote
+        vote_history.add_vote_history("user_1", "project_a/topic_aaa", "192.168.0.3")
+        self.assertEqual(
+            [Decimal(1111), Decimal(2222), Decimal(9999)],
+            vote_history.get_vote_history("user_1", "project_a/topic_aaa"),
+        )
+
+        # New vote
+        vote_history.add_vote_history("user_1", "project_a/topic_ccc", "192.168.0.3")
+        self.assertEqual(
+            [Decimal(9999)],
+            vote_history.get_vote_history("user_1", "project_a/topic_ccc"),
+        )
+
+    def test_check_ip_voted(self):
+        vote_history = VoteHistory()
+
+        # Valid vote
+        self.assertFalse(
+            vote_history.check_ip_voted("user_1", "project_a/topic_aaa", "192.168.0.3")
+        )
+
+        # Invalid vote
+        self.assertTrue(
+            vote_history.check_ip_voted("user_1", "project_a/topic_aaa", "192.168.0.1")
+        )
+
+        # New vote
+        self.assertFalse(
+            vote_history.check_ip_voted("user_1", "project_a/topic_ccc", "192.168.0.1")
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
