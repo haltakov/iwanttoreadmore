002a82c309b1b4ad87ff56dbc5fa4f69f30d3e8f
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2020-12-06 01:32:34 +0100

    Change A to BUTTON on the stats page

--- website/themes/minimal/static/js/stats.js
+++ website/themes/minimal/static/js/stats.js
@@ -73,16 +73,16 @@ function fillVotesTable(user, votesData) {
         voteRow.insertAdjacentHTML(
             "beforeend",
             `<td>
-                <a href="#" class="link vote-config">
+                <button class="link vote-config">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                </a>
-                <div class="vote-config-menu absolute right-0 mr-3 hidden rounded-lg shadow-lg uppercase text-sm font-bold text-center text-gray-700 bg-white">
-                    <a class="vote-config-hide rounded-lg rounded-b-none hover:bg-gray-200 border border-3 border-gray-400 w-32 p-2 block" href="#">Hide</a>
-                    <a class="vote-config-show rounded-lg rounded-b-none hover:bg-gray-200 border border-3 border-gray-400 w-32 p-2 block hidden" href="#">Show</a>
-                    <a class="vote-config-delete rounded-lg rounded-t-none hover:bg-gray-200 border border-3 border-gray-400 w-32 p-2 block border-t-0" href="#">Delete</a>
+                </button>
+                <div class="vote-config-menu absolute right-0 mr-3 hidden rounded-lg shadow-lg text-center text-gray-700 bg-white">
+                    <button class="vote-config-hide rounded-lg rounded-b-none hover:bg-gray-200 uppercase text-sm font-bold border border-3 border-gray-400 w-32 p-2 block">Hide</button>
+                    <button class="vote-config-show rounded-lg rounded-b-none hover:bg-gray-200 uppercase text-sm font-bold border border-3 border-gray-400 w-32 p-2 block hidden">Show</button>
+                    <button class="vote-config-delete rounded-lg rounded-t-none hover:bg-gray-200 uppercase text-sm font-bold border border-3 border-gray-400 w-32 p-2 block border-t-0">Delete</button>
                 </div>
             </td>`
         );
@@ -215,6 +215,8 @@ function initVoteMenus(table) {
             }
 
             event.stopPropagation();
+
+            return false;
         });
     });
 
@@ -245,6 +247,8 @@ function initVoteMenus(table) {
                 credentials: "same-origin",
                 body: "1",
             });
+
+            return false;
         });
     });
 
@@ -266,6 +270,8 @@ function initVoteMenus(table) {
                 credentials: "same-origin",
                 body: "0",
             });
+
+            return false;
         });
     });
 
@@ -284,6 +290,8 @@ function initVoteMenus(table) {
             });
 
             row.remove();
+
+            return false;
         });
     });
 
