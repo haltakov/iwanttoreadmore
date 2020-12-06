2290610ab4f31a031308ce0d911ded3c9740f673
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2020-12-06 01:37:57 +0100

    Hide vote menu if the user is not logged in

--- website/themes/minimal/static/js/stats.js
+++ website/themes/minimal/static/js/stats.js
@@ -70,9 +70,11 @@ function fillVotesTable(user, votesData) {
             "beforeend",
             `<td class="py-1 text-center" data-value="${vote.last_vote}">${timestampToDate(vote.last_vote)}</td>`
         );
-        voteRow.insertAdjacentHTML(
-            "beforeend",
-            `<td>
+
+        if (document.cookie.endsWith(`=${user}`)) {
+            voteRow.insertAdjacentHTML(
+                "beforeend",
+                `<td>
                 <button class="link vote-config">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
@@ -85,7 +87,10 @@ function fillVotesTable(user, votesData) {
                     <button class="vote-config-delete rounded-lg rounded-t-none hover:bg-gray-200 uppercase text-sm font-bold border border-3 border-gray-400 w-32 p-2 block border-t-0">Delete</button>
                 </div>
             </td>`
-        );
+            );
+        } else {
+            voteRow.insertAdjacentHTML("beforeend", `<td></td>`);
+        }
 
         tbody.appendChild(voteRow);
     });
